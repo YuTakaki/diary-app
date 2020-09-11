bce81e84ad4be33ce526ed30abcf275d92d75189
YuTakaki <yu.takaki2000@gmail.com>
2020-09-11 17:13:36 +0900

    heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/home/home';
 import Navbar from './components/nav/nav';
 import AuthProvider from './context/authentication';
