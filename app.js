f1280cd31949dfdc058ba463836dc662b264f30e
YuTakaki <yu.takaki2000@gmail.com>
2020-09-11 17:05:23 +0900

    heroku deploy

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ mongoose.Promise = global.Promise;
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('connected to database'))
     .catch(error => console.log(err));
 require('./setup/passport')(passport);
@@ -24,6 +24,10 @@ app.use(passport.session());
 
 app.use('/dashboard', require('./route/dashboard'));
 app.use('/user', require('./route/login_signup'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+}
 
 const port = process.env.port || 4000;
 app.listen(port, () => console.log(`connected to port ${port}`))
